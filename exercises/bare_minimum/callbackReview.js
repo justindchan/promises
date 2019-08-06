97ad76049022382bf2fce16795956da453017dfc
Hack Reactor Student <student@hackreactor.com>
2019-08-05 20:06:16 -0700

    completed first line pluck and get status code, onto Promises refactor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,30 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, fileData) => {
+    if(err) {
+      callback(err);
+    } else {
+      var fileText = fileData.toString();
+      fileText = fileText.substring(0, fileText.indexOf("\n"));
+      callback(null, fileText);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+
+  request(url, (error, response, body) => {
+    if(error) {
+      callback(error);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
